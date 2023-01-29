a36a85aba1f46f568256c3d5f87327dc1d5152b5
Loise <loise.wambui@student.moringaschool.com>
2023-01-29 23:04:50 +0300

    Added  YourBotArmy

---
+++ src/Components/YourBotArmy.js
@@ -0,0 +1,24 @@
+import React,  {useEffect} from "react";
+//import BotSpecs from "./BotSpecs";
+import BotCard2 from "./BotCard2";
+
+function YourBotArmy({botsArmy, setBotsArmy}) {
+  //your bot army code here...
+const botsArmyList = botsArmy.map(bot=>{
+  return <BotCard2 key={bot.id} bot={bot} botsArmy={botsArmy} setBotsArmy={setBotsArmy}/>
+})
+
+  return (
+    <div className="ui segment inverted olive bot-army">
+      <div className="ui five column grid">
+      <h4>Your Bot Army</h4>
+        <div className="row bot-army-row">
+          {botsArmyList}
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default YourBotArmy;
