717f1b8a97f2021e26189f393c20dddca7fbed6a
Loise <loise.wambui@student.moringaschool.com>
2023-01-29 22:51:51 +0300

    Add App.js

---
+++ src/Components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import BotsPage from "./BotsPage";
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import BotSpecs from "./BotSpecs";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<BotsPage />} />
+          <Route path="/:id" element={<BotSpecs />}/>
+        </Routes>
+        
+      </div>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
