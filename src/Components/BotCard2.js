505efab2e7a62d02d576878e98ba8c44e320eb7c
Loise <loise.wambui@student.moringaschool.com>
2023-01-29 22:58:23 +0300

    Added BotCard2

---
+++ src/Components/BotCard2.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+const botTypeClasses = {
+  Assault: "icon military",
+  Defender: "icon shield",
+  Support: "icon plus circle",
+  Medic: "icon ambulance",
+  Witch: "icon magic",
+  Captain: "icon star",
+};
+
+function BotCard2({ bot, botsArmy, setBotsArmy}) {
+  // add bot to bots army
+  function handleBotClick(){
+    //console.log(bot.id);
+    const remainingBotsArmy = botsArmy.filter(botArmy=> botArmy.id !== bot.id);
+    setBotsArmy (remainingBotsArmy)
+  }
+
+  
+  return (
+    <div className="ui column">
+      <div
+        className="ui card"
+        key={bot.id}
+        onClick={handleBotClick}
+      >
+        <div className="image">
+          <img alt="oh no!" src={bot.avatar_url} />
+        </div>
+        <div className="content">
+          <div className="header">
+            {bot.name}
+            <i className={botTypeClasses[bot.bot_class]} />
+          </div>
+          <div className="meta text-wrap">
+            <small>{bot.catchphrase}</small>
+          </div>
+        </div>
+        <div className="extra content">
+          <span>
+            <i className="icon heartbeat" />
+            {bot.health}
+          </span>
+
+          <span>
+            <i className="icon lightning" />
+            {bot.damage}
+          </span>
+          <span>
+            <i className="icon shield" />
+            {bot.armor}
+          </span>
+          {/* <span>
+            <div className="ui center aligned segment basic">
+              <button
+                className="ui mini red button"
+                onClick={() =>
+                  console.log("add code to connect event listener")
+                }
+              >
+                x
+              </button>
+            </div>
+          </span> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BotCard2;
