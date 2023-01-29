4d308ebdc1247302fa9e26d4e042f85690fd79ab
Loise <loise.wambui@student.moringaschool.com>
2023-01-29 22:59:28 +0300

    Added BotCollection

---
+++ src/Components/BotCollection.js
@@ -0,0 +1,30 @@
+import React, {useEffect} from "react";
+import BotCard from "./BotCard";
+
+function BotCollection({bots, setBots, botsArmy, setBotsArmy}) {
+  // Your code here
+
+  //fetch the bots
+  useEffect(()=>{
+    fetch('http://localhost:8002/bots')
+    .then(res=>res.json())
+    .then(data=>{
+      setBots(data);
+    })
+  }, []);
+
+  const newBots = bots.map(bot=>{
+    return <BotCard key={bot.id} bot={bot} botsArmy={botsArmy} setBotsArmy={setBotsArmy} bots={bots} setBots={setBots}/>
+  })
+
+  return (
+    <div className="ui four column grid">
+      <div className="row">
+        {newBots}
+        Collection of all bots
+      </div>
+    </div>
+  );
+}
+
+export default BotCollection;
